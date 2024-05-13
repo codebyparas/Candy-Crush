3f12e0ec84c61fb471ed85a6c64479f89fb17933
Paras-Sachdeva <paras140902@gmail.com>
2024-05-13 23:42:28 +0530

    FEAT: Drap & Drop Candy

--- candy.js
+++ candy.js
@@ -4,6 +4,9 @@ let rows = 9;
 let columns = 9;
 let score = 0;
 
+let currTile;
+let otherTile;
+
 window.onload = function(){
     startGame();
 }
@@ -21,6 +24,14 @@ function startGame(){
             tile.id = r.toString() + "-" + c.toString();
             tile.src = "./Images/" + randomCandy() + ".png";
 
+            // Drag & Drop Functionality
+            tile.addEventListener("dragstart", dragStart);  // Click on a Candy, Initialize Drag Process
+            tile.addEventListener("dragover", dragOver);  // Clicking on Candy, Moving Mouse to Drag the Candy
+            tile.addEventListener("dragenter", dragEnter);  // Dragging Candy onto Another Candy
+            tile.addEventListener("dragleave", dragLeave);  // Leave Candy over Anoter Candy
+            tile.addEventListener("drop", dragDrop);  // Dropping a Candy over Another Candy
+            tile.addEventListener("dragend", dragEnd);  // After Drag Process Completed, We Swap candies
+
             document.getElementById("board").append(tile);
             row.push(tile);
         }
@@ -28,4 +39,32 @@ function startGame(){
     }
 
     console.log(board);
+}
+
+function dragStart(){
+    // this refers to Tile that we Clicked on for Dragging
+    currTile = this;
+}
+
+function dragOver(e){
+    e.preventDefault();
+}
+
+function dragEnter(e){
+    e.preventDefault();
+}
+
+function dragLeave(){
+}
+
+function dragDrop(){
+    // this refers to the Target that was Dropped on 
+    otherTile = this;
+}
+
+function dragEnd(){
+    let currImg = currTile.src;
+    let otherImg = otherTile.src;
+    currTile.src = otherImg;
+    otherTile.src = currImg;
 }
